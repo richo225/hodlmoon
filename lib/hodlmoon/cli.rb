a5707af32f5329e8d67ff3e62e1be29525dfb4d9
Richard Bates <richard.bates@babylonhealth.com>
2018-02-08 21:36:18 +0000

    Add rubocop and fix offenses

--- lib/hodlmoon/cli.rb
+++ lib/hodlmoon/cli.rb
@@ -9,12 +9,12 @@ module Hodlmoon
       eur: '€'
     }.freeze
 
-    desc "hello NAME", "say hello to NAME"
+    desc 'hello NAME', 'say hello to NAME'
     def hello(name)
       puts "Hello #{name}"
     end
 
-    desc "price COIN CURRENCY", "get current price of COIN in CURRENCY"
+    desc 'price COIN CURRENCY', 'get current price of COIN in CURRENCY'
     def price(coin, currency)
       price = Hodlmoon::Client::CoinMarketCap.retrieve_price(coin, currency)
       puts "#{CURRENCY[currency.to_sym]}#{price}"
